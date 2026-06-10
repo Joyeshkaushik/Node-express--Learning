9239c194a28bdd71cd0326f3441679dc5df3f9db
Joyesh Kaushik <173632815+Joyeshkaushik@users.noreply.github.com>
2026-06-10 22:51:42 +0530

    Implemented a short project of url shortening

---
+++ SHORT-URL/routes/url.js
@@ -0,0 +1,6 @@
+const express=require("express");
+const router=express.Router();
+const {handleGenerateNewShortURl,handleGetAnalytics} =require("../controllers/url");
+router.post("/",handleGenerateNewShortURl);
+router.get("/analytics/:shortId",handleGetAnalytics);
+module.exports=router;
