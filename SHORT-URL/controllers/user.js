c494ea3c7b93f827e7697224dc9aebae32bf14a5
Joyesh Kaushik <173632815+Joyeshkaushik@users.noreply.github.com>
2026-06-11 21:24:17 +0530

    Implementing domain specific cookie which can be only accesed by specific domain

--- SHORT-URL/controllers/user.js
+++ SHORT-URL/controllers/user.js
@@ -18,7 +18,9 @@ async function handleUserLogin(req,res){
     });
     
     const token=setUser(user);
-     res.cookie("uid",token);
+     res.cookie("uid",token,{
+        domain:"www.google.com",
+     });
     return res.redirect("/");
 }
 module.exports={
