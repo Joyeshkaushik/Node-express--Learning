d8921d15163bdb3c29cedad29ae5faa289837534
Joyesh Kaushik <173632815+Joyeshkaushik@users.noreply.github.com>
2026-06-11 22:21:23 +0530

    Learning another way of sending tokens to user and sending the token to server in header .There is one authoriation header in whihc token is send

--- SHORT-URL/controllers/user.js
+++ SHORT-URL/controllers/user.js
@@ -18,10 +18,8 @@ async function handleUserLogin(req,res){
     });
     
     const token=setUser(user);
-     res.cookie("uid",token,{
-        domain:"www.google.com",
-     });
-    return res.redirect("/");
+    
+    return res.json({token});
 }
 module.exports={
     handleUserSignup,
--- SHORT-URL/middlewares/auth.js
+++ SHORT-URL/middlewares/auth.js
@@ -1,16 +1,23 @@
 const {getUser} =require("../service/auth");
 async function restrictToLoggedinUserOnly(req,res,next){
-    const userUid=req.cookies.uid;
+    const userUid=req.headers["authorization"];
+
+    console.log(req.headers);
     if(!userUid) return res.redirect("/login");
-    const user=getUser(userUid);
+    const token=userUid.split("Bearer")[1];
+    const user=getUser(token);
     if(!user) return res.redirect("/login");
     req.user=user;
     next();
 }
 async function checkAuth(req,res,next){
-    const userUid=req.cookies.uid;
-  
-    const user=getUser(userUid);
+      
+     const userUid=req.headers["authorization"];
+     if (!userUid) {
+    return res.status(401).send("Unauthorized");
+}
+  const token=userUid.split(" ")[1];
+    const user=getUser(token);
     
     req.user=user;
     next();
